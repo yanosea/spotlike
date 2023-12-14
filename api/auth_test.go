1048e64d1686a32466aa5494c584d27b83cbdcd6
yanosea <myanoshi0626@gmail.com>
2023-12-14 17:48:19 +0900

    wip

--- api/auth_test.go
+++ api/auth_test.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 	"reflect"
+	"strings"
 	"testing"
 
 	"github.com/zmb3/spotify/v2"
@@ -10,21 +12,45 @@ import (
 
 func TestGetClient(t *testing.T) {
 	tests := []struct {
-		name    string
-		want    *spotify.Client
-		wantErr bool
+		name               string
+		want               *spotify.Client
+		wantErr            bool
+		wantSetAuthInfoErr bool
+		wantMsg            string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "positive testing",
+			want: &spotify.Client{},
+		},
+		{
+			name:               "negative testing (setAuthInfo error)",
+			wantErr:            true,
+			wantSetAuthInfoErr: true,
+			wantMsg:            "fake setAuthInfo error",
+		},
 	}
 	for _, tt := range tests {
+		if tt.wantSetAuthInfoErr {
+			oldSetAuthInfo := setAuthInfo
+			setAuthInfo = setAuthInfoFake
+			defer func() { setAuthInfo = oldSetAuthInfo }()
+		}
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := GetClient()
-			if (err != nil) != tt.wantErr {
-				t.Errorf("GetClient() error = %v, wantErr %v", err, tt.wantErr)
+			if err == nil && reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
+				t.Errorf("GetClient() = %v, want = %v", got, tt.want)
 				return
 			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetClient() = %v, want %v", got, tt.want)
+			if err != nil && tt.wantErr {
+				msg := err.Error()
+				if !strings.Contains(msg, tt.wantMsg) {
+					t.Errorf("GetClient() unexpected error = %v, wantErr %v", err, tt.wantErr)
+					return
+				}
+			}
+			if err != nil && !tt.wantErr {
+				t.Errorf("GetClient() unexpected error = %v, wantErr %v", err, tt.wantErr)
+				return
 			}
 		})
 	}
@@ -34,9 +60,7 @@ func Test_setAuthInfo(t *testing.T) {
 	tests := []struct {
 		name    string
 		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := setAuthInfo(); (err != nil) != tt.wantErr {
@@ -51,9 +75,7 @@ func Test_authenticate(t *testing.T) {
 		name    string
 		want    *spotify.Client
 		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := authenticate()
@@ -76,12 +98,14 @@ func Test_completeAuthenticate(t *testing.T) {
 	tests := []struct {
 		name string
 		args args
-	}{
-		// TODO: Add test cases.
-	}
+	}{}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			completeAuthenticate(tt.args.w, tt.args.r)
 		})
 	}
+}
+
+func setAuthInfoFake() error {
+	return errors.New("fake setAuthInfo error")
 }
